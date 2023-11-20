4e62ecca0e478160e35cff29426f131d117bf417
jacop <jacop@DESKTOP-PTJNHA6>
2023-11-20 18:04:52 +0100

    Creare una classe chiamata "Esempio" con gli attributi "stringa e "numero", completa di costruttori, metodi accessor, distruttore e Dispose. Creare un'istanza della classe "Esempio" nel main che mostri il funzionamento dei distruttori.
    P.S
    esiste una classe che gestisce il garbage collector chiamata GC che ha un metodo chiamato Collect() ......

--- 3 - prova distruttore/Program.cs
+++ 3 - prova distruttore/Program.cs
@@ -8,10 +8,72 @@ namespace _3___prova_distruttore
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Creazione di un'istanza della classe "Esempio"
+            using (Esempio esempio = new Esempio("Hello", 42))
+            {
+                // Utilizzo dell'istanza
+                Console.WriteLine("Stringa: {0}, Numero: {1}", esempio.GetStringa(), esempio.GetNumero());
+            } // Qui verrà chiamato automaticamente Dispose() quando esce dal blocco using
 
+            // Chiamata esplicita al Garbage Collector
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
 
+            Console.WriteLine("Fine del programma.");
         }
     }
+
+    class Esempio : IDisposable
+    {
+        private string stringa;
+        private int numero;
+
+        // Costruttore
+        public Esempio(string inputStringa, int inputNumero)
+        {
+            stringa = inputStringa;
+            numero = inputNumero;
+            Console.WriteLine("Oggetto creato. Stringa: {0}, Numero: {1}", stringa, numero);
+        }
+
+        // Metodo accessor per la stringa
+        public string GetStringa()
+        {
+            return stringa;
+        }
+
+        // Metodo accessor per il numero
+        public int GetNumero()
+        {
+            return numero;
+        }
+
+        // Metodo Dispose
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Distruttore
+        ~Esempio()
+        {
+            Dispose(false);
+        }
+
+        // Implementazione del metodo Dispose
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Rilascia le risorse gestite
+                Console.WriteLine("Risorse gestite rilasciate.");
+            }
+
+            // Rilascia le risorse non gestite
+            Console.WriteLine("Risorse non gestite rilasciate.");
+        }
+    }    
 }
